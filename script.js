94103e1ba978aa8f780442258d3d0e6b4fdaf785
NehaNayak123 <111532433+NehaNayak123@users.noreply.github.com>
2025-02-23 21:25:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 function trailingZeros(n) {
   //your JS code here. If required.
+	if(n==0){return 0}
+	let factorial=1
+	for(let i=1;i<=n;i++){
+		factorial*=i;
+	}
+// 	console.log(factorial)
+	let count=0;
+	while(factorial>0){
+		if(factorial%10==0){
+			count++;
+			factorial=parseInt(factorial/10)
+		}
+		else{
+			break;
+		}
+	}
+	return count;
 }
 
 const input = prompt("Enter a number");
